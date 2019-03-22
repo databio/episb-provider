6c3a52e2cf68e5d5a8715ed5a9439e409899865d
OD <oddodaoddo@gmail.com>
2019-03-22 08:19:47 -0400

    Switch off debugging log output when writing experiments. Change docker file to reflect current version of app

--- episb-utils/src/main/scala/com/github/oddodaoddo/sheffieldapp/datawriter/DataWriter.scala
+++ episb-utils/src/main/scala/com/github/oddodaoddo/sheffieldapp/datawriter/DataWriter.scala
@@ -36,7 +36,7 @@ trait JSONWriter extends LazyLogging {
 class ElasticSearchWriter(index:String, subIndex:String) extends JSONWriter with ElasticConnector {
   def write(data:String):Either[String,Boolean] = {
     try {
-      logger.debug(s"(ElasticSearchWriter::write): data=${data}")
+      //logger.debug(s"(ElasticSearchWriter::write): data=${data}")
       val esresponse:IndexResponse = esclient.prepareIndex(index, subIndex).
             setSource(data, XContentType.JSON).get()
       Right(true)
@@ -48,7 +48,7 @@ class ElasticSearchWriter(index:String, subIndex:String) extends JSONWriter with
   // write any JSONLDable class descendent into elastic
   // return how many items were written
   def write(data:List[JSONLDable]):Either[String,Boolean] = {
-    logger.info(s"(ElasticSearchWriter::write): data=${data}")
+    //logger.info(s"(ElasticSearchWriter::write): data=${data}")
     if (data.isEmpty) Left("Empty list, skipped commital")
     else {
       try {
@@ -72,7 +72,7 @@ class ElasticSearchWriter(index:String, subIndex:String) extends JSONWriter with
 // creates new file on every class instantiation
 class LocalFileWriter(path:String) extends JSONWriter {
   def write(data:String): Either[String, Boolean] = {
-    logger.debug(s"(LocalFileWriter::write): data=${data}")
+    //logger.debug(s"(LocalFileWriter::write): data=${data}")
     try {
       val f: FileWriter = new FileWriter(path)
       f.write(data) // write to local file
@@ -84,7 +84,7 @@ class LocalFileWriter(path:String) extends JSONWriter {
   }
 
   def write(data:List[JSONLDable]):Either[String,Boolean] = {
-    logger.debug(s"(LocalFileWriter::write): data=${data}")
+    //logger.debug(s"(LocalFileWriter::write): data=${data}")
     if (data.isEmpty) Left("Empty list, skipped commital")
     else {
       write(data.map(_.toJsonLD).mkString("\n"))
