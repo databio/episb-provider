0b06da774d859cdadc7e5b46446832e463b9fbb9
OD <oddodaoddo@gmail.com>
2019-05-20 12:13:33 -0400

    Add query to get all experiments by name and optional range

--- episb-provider-fastapi/main.py
+++ episb-provider-fastapi/main.py
@@ -106,3 +106,36 @@ async def listSegmentations():
     finally:
         if cur is not None:
             cur.close()
+
+# get all annotation values by experiment name
+# optional parameters are operations >/</= and values
+@app.get("/experiments/get/ByName/:expName")
+async def getAnnotationsByExperimentName(expName:str, op1:str=None, op2:str=None, val1:float=None, val2:float=None):
+    # basic query below
+    sqlq = """SELECT * FROM annotations WHERE exp_name = %s"""
+    # add up the rest of the query if parameters were passed in
+    if op1 is not None and val1 is not None:
+        sqlq_ap1 = """ AND value %s %f """ % (op1, val1)
+        sqlq += sqlq_ap1
+    if op2 is not None and val2 is not None:
+        sqlq_ap2 = """ AND value %s %f """ % (op2, val2)
+        sqlq += sqlq_ap2
+    res = []
+    try:
+        # use a server side cursor to speed things up
+        cur = conn.cursor('server_side_cursor')
+        cur.execute(sqlq, [expName])
+        while True:
+            # fetch in 1000 increments
+            rows = cur.fetchmany(1000)
+            if not rows:
+                break
+            res.append(rows)
+        return {"message": res}
+    except psycopg2.Error as error:
+        return {"error": error.pgerror}
+    except Exception as e:
+        return {"error": e.args[0]}
+    finally:
+        if cur is not None:
+            cur.close()
