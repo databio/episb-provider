7e6c468a6ca0e2ca573b0593986b3e8da2a422f9
OD <oddodaoddo@gmail.com>
2019-05-20 18:16:13 -0400

    Add query to get all annotations for a given segmentation

--- episb-provider-fastapi/main.py
+++ episb-provider-fastapi/main.py
@@ -2,6 +2,8 @@ from datastructures import *
 from postgres import conn
 from fastapi import FastAPI
 import psycopg2
+from starlette.responses import FileResponse
+import tempfile
 
 app = FastAPI()
 
@@ -132,6 +134,37 @@ async def getAnnotationsByExperimentName(expName:str, op1:str=None, op2:str=None
                 break
             res.append(rows)
         return {"message": res}
+    except psycopg2.Error as error:
+        return {"error": error.pgerror}
+    except Exception as e:
+        return {"error": e.args[0]}
+    finally:
+        if cur is not None:
+            cur.close()
+
+@app.get("/experiments/get/BySegmentationName/:segName")
+async def getAnnotationsBySegmentationName(segName:str, matrix:bool=None):
+    # basic query below
+    sqlq = """SELECT * FROM annotations WHERE segmentation_name = %s"""
+    # add up the rest of the query if parameters were passed in
+    if matrix is not None:
+        # here we serve the results as a .gz file
+        sqlq_ap2 = """ GROUP BY exp_name"""
+        sqlq += sqlq_ap2
+    try:
+        # use a server side cursor to speed things up
+        cur = conn.cursor('server_side_cursor')
+        cur.execute(sqlq, [segName])
+        # create the output file
+        with tempfile.NamedTemporaryFile(mode="w", suffix=".json", delete=False) as f_out:
+            while True:
+                # fetch in 1000 increments
+                rows = cur.fetchmany(1000)
+                if not rows:
+                    break
+                for item in rows:
+                    f_out.write(','.join(map(str, item))+'\n')
+            return FileResponse(f_out.name, media_type="text/plain")
     except psycopg2.Error as error:
         return {"error": error.pgerror}
     except Exception as e:
